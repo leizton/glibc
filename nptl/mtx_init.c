0ab99514c66ae43049ee7837a49b6b0b056ee814
leizton <leizton@126.com>
2024-06-05 10:08:59 +0800

    format

--- nptl/mtx_init.c
+++ nptl/mtx_init.c
@@ -20,34 +20,31 @@
 
 #include "thrd_priv.h"
 
-int
-mtx_init (mtx_t *mutex, int type)
-{
-  _Static_assert (sizeof (mtx_t) == sizeof (pthread_mutex_t),
-		  "sizeof (mtx_t) != sizeof (pthread_mutex_t)");
-  _Static_assert (alignof (mtx_t) == alignof (pthread_mutex_t),
-		  "alignof (mtx_t) != alignof (pthread_mutex_t)");
+int mtx_init(mtx_t* mutex, int type) {
+  _Static_assert(sizeof(mtx_t) == sizeof(pthread_mutex_t),
+                 "sizeof (mtx_t) != sizeof (pthread_mutex_t)");
+  _Static_assert(alignof(mtx_t) == alignof(pthread_mutex_t),
+                 "alignof (mtx_t) != alignof (pthread_mutex_t)");
 
   pthread_mutexattr_t attr;
 
-  __pthread_mutexattr_init (&attr);
+  __pthread_mutexattr_init(&attr);
 
   /* Another possible solution would be to set the flags directly in
      mutex object. */
-  switch (type)
-  {
-    case mtx_plain | mtx_recursive:
-    case mtx_timed | mtx_recursive:
-      __pthread_mutexattr_settype (&attr, PTHREAD_MUTEX_RECURSIVE);
-      break;
-    case mtx_plain:
-    case mtx_timed: /* No difference between both in standard */
-    default:
-      __pthread_mutexattr_settype (&attr, PTHREAD_MUTEX_NORMAL);
-      break;
+  switch (type) {
+  case mtx_plain | mtx_recursive:
+  case mtx_timed | mtx_recursive:
+    __pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+    break;
+  case mtx_plain:
+  case mtx_timed: /* No difference between both in standard */
+  default:
+    __pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_NORMAL);
+    break;
   }
 
-  int err_code = __pthread_mutex_init ((pthread_mutex_t *) mutex, &attr);
+  int err_code = __pthread_mutex_init((pthread_mutex_t*)mutex, &attr);
   /* pthread_mutexattr_destroy implementation is a noop.  */
-  return thrd_err_map (err_code);
+  return thrd_err_map(err_code);
 }
